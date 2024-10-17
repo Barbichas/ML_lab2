062402a71b429e1feea202e853562cb08c1aba9f
Barbichas <oeinsteineracareca@gmail.com>
2024-10-17 13:42:09 +0100

    temos uma CNN, nao grande coisa

--- classify_SVM.py
+++ classify_SVM.py
@@ -5,7 +5,9 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+
+random.seed(42)
 
 X_train = np.load("Xtrain1.npy")
 y_train = np.load("Ytrain1.npy")
@@ -14,7 +16,7 @@ X_test  = np.load("Xtest1.npy")
 ####################################################################
 ### define the validation and training sets  #######################
 ####################################################################
-percent_val = 50
+percent_val = 30
 n_val = int(percent_val * len(X_train) / 100)
 
 print("Use " + str(n_val) + " images for validation")
@@ -37,7 +39,7 @@ print()
 print("DESCRIPTION OF DATASET")
 print()
 print("Number of training images = " + str(train_total))
-print("Number of validation images = " +str(X_val.shape[0]))
+print("Number of validation images = " +str(X_val.shape[0]) + str(" ( ") + str(percent_val) + " %)")
 print("Number of craters = " + str(n_craters))
 print("Number of plain= " + str(n_plain))
 print("Percentage of craters is " + str(100*n_craters/train_total) + " %")
@@ -55,7 +57,7 @@ def rotate_image_Sofia(image):
         image[ii] = aux
     return image
 
-####### Function to brightness variations image  ###########
+####### Function for brightness variations image  ###########
 def bright_image(image):
     fator_brilho = 1.5
     image_bright = np.clip(image * fator_brilho, 0, 255)#.astype(np.uint8)
@@ -64,6 +66,17 @@ def bright_image(image):
     #cv2.imshow('Imagem com Mais Brilho', image_bright)
 
     return image_bright
+
+####### Function for transposing the image   ###########
+def transpose_image(image):
+    matrix = np.array(image).reshape(48, 48)
+    transpose = np.transpose(matrix)
+    return np.array(transpose).reshape(48**2)
+
+#######  Function for negative           ############
+def negative_image(image):
+    negative = [255-pixel for pixel in image ]
+    return np.array(negative)
 
 ########   Even number of craters and plains  ##############
 def equalize_crat_and_plain(X, y):
@@ -81,7 +94,7 @@ def equalize_crat_and_plain(X, y):
 
         if len(craters) == len(plains) :
             break
-        #dd random plain 
+        #add random plain 
         aux = plains[random.randint(0, len(plains)-1)]
         aux = bright_image(aux)
         plains.append(aux)
@@ -103,8 +116,42 @@ def equalize_crat_and_plain(X, y):
 
     return X_final, y_final
 
+def add_transpose_images(X,y):
+    more_X = []
+    for image in X:
+        more_X.append(transpose_image(image))
+    more_X = np.array(more_X)
+    X_final = np.concatenate( (X,more_X) )
+    y_final = np.concatenate( (y,y) )
+
+    return X_final , y_final
+
+def add_bright_images(X,y):
+    more_X = []
+    for image in X:
+        more_X.append(bright_image(image))
+    more_X = np.array(more_X)
+    X_final = np.concatenate( (X,more_X) )
+    y_final = np.concatenate( (y,y) )
+
+    return X_final , y_final
+
+def add_negative_images(X,y):
+    more_X = []
+    for image in X:
+        more_X.append(bright_image(image))
+    more_X = np.array(more_X)
+    X_final = np.concatenate( (X,more_X) )
+    y_final = np.concatenate( (y,y) )
+
+    return X_final , y_final
+
+
 print("Equalize number of images")
 X_train, y_train = equalize_crat_and_plain(X_train,y_train)
+#X_train, y_train = add_transpose_images(X_train,y_train)
+#X_train, y_train = add_bright_images(X_train, y_train)
+#X_train , y_train = add_negative_images(X_train, y_train)
 
 
 
@@ -128,22 +175,7 @@ print("Number of plain= " + str(n_plain))
 print("Percentage of craters is " + str(100*n_craters/train_total) + " %")
 print("Percentage of plain is " + str(100*n_plain/train_total) + " %")
 
-
-####       Normalize       #################################
-X_train_means = np.mean(X_train,axis = 0)    #Important for finale!
-X_train_centered = X_train - X_train_means
-X_train_centered_maxs = np.max(np.abs(X_train_centered), axis=0)  # Important for finale!
-X_train_normalised = X_train_centered / X_train_centered_maxs
-X_train_normalised_std_devs = np.std(X_train_centered, axis=0 ) #Important for finale!
-X_train_normalised = X_train_normalised/ X_train_normalised_std_devs
-
-y_train_mean = np.mean(y_train)          #Important for finale!
-y_train_centered = y_train - y_train_mean
-y_train_centered_max = np.max(y_train_centered)
-y_train_normalised = y_train_centered / y_train_centered_max
-y_train_normalised_std_dev = np.std(y_train_centered) #use standard deviation to normalise gaussian noise
-y_train_normalised = y_train_normalised/ y_train_normalised_std_dev
-
+######  looking at  images  ############
 def display_image(vector, L, H):
     # Convert the vector to a 2D array (image) with shape (H, L)
     image = np.array(vector).reshape(H, L)
@@ -152,12 +184,11 @@ def display_image(vector, L, H):
     plt.imshow(image, cmap='gray')  # Using 'gray' colormap for grayscale images
     plt.axis('off')  # Hide axes
 
-display_image(X_train[0], 48 ,48)
-plt.figure()
-display_image( bright_image(X_train[0] ), 48 ,48)
-plt.figure()
+#display_image(X_train[0], 48 ,48)
+#plt.figure()
+#display_image( negative_image(X_train[0] ), 48 ,48)
+#plt.figure()
 
-###  look at some images  ###
 if(0):
     for i in range(20):
         display_image(X_train[i],48,48)
@@ -168,65 +199,105 @@ if(0):
             plt.title("Image "+ str(i) + " is clean")
         plt.figure()
 
+###############################################################################
+#######                Go to feature space                                #####
+###############################################################################
+from skimage.feature import hog
+from skimage import exposure
+def compute_hog(image):
+    """ Compute HOG features and return them along with the visualized image. """
+    features, hog_image = hog(image, 
+                               orientations=6, 
+                               pixels_per_cell=(48, 48), 
+                               cells_per_block=(1, 1), 
+                               visualize=True)
+    # Rescale histogram for better display
+    hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
+    return features, hog_image
+
+
+features_train = []
+if(1):
+    for i_train in range(len(X_train)) :
+        if(i_train % 400 == 0):
+            print("Turning to features input image number " + str(i_train))
+        features_train.append( compute_hog(np.array(X_train[i_train]).reshape(48, 48))[0] )
+    features_train = np.array( features_train )
+    np.save("SVM_features.npy",features_train)
+
+if(0): #loading input features from file
+    features_train = np.load("SVM_features.npy")
+
+
 ################################################################################
-###############              Decision Tree
-###############################################################################3
+##########           Support Vector Machine                    #################
+################################################################################
+
+
 print()
 print("----------------------------")
-print("Random Decision Tree")
+print("Support Vector Machine")
 print()
 
-random_state_max = 50
+svm = SVC(kernel='linear')
+y_val_pred=[]
+if(1): #use input space
+    print("Using input space")
+    svm.fit(X_train, y_train)
+    y_val_pred = svm.predict(X_val)
+if(0): #use feature space
+    print("Using feature space")
+    #train model
+    svm.fit(features_train , y_train)
+    #pass validation set to feature space
+    features_val = []
+    for i_val in range(len(X_val)) :
+        if(i_val % 200 == 0):
+            print("Turning to features validation image number " + str(i_val))
+        features_val.append( compute_hog(np.array(X_train[i_val]).reshape(48, 48))[0] )
+    features_val = np.array( features_val )
+    y_val_pred = svm.predict(features_val)
+if(0): #use both input and feature space
+    print("Using input AND feature space, lord have mercy on this computer")
+    for i in range(len(features_train)):
+        i = int(i)
+        features_train[i] = np.concatenate((features_train[i],X_train[i]))
+    mix_train = features_train
+    svm.fit(mix_train, y_train)
+    #pass validation set to feature space
+    features_val = []
+    for i_val in range(len(X_val)) :
+        if(i_val % 200 == 0):
+            print("Turning to features validation image number " + str(i_val))
+        features_val.append( compute_hog(np.array(X_train[i_val]).reshape(48, 48))[0] )
+    features_val = np.array( features_val )
+    y_val_pred = svm.predict(features_val)
+    #mix output
+    for i in range(len(features_val)):
+        i = int(i)
+        features_val[i] = np.concatenate((features_val[i],X_val[i]))
+    mix_val = features_val
+    y_val_pred = svm.predict(mix_val)
+    
 
-f1_train = []
-f1_val = []
-if (0):
-    for rs in range(1,random_state_max):
-        if(rs%5==0):
-            print("Number of random states = " + str(rs) )
-        clf = DecisionTreeClassifier(random_state=rs)
-        clf.fit(X_train, y_train)
 
-        y_pred = clf.predict(X_train)
-        TP = 0
-        FN = 0
-        FP = 0
-        for i in range(len(y_pred)):
-            delta = y_train[i] - y_pred[i]
-            if delta == 0:
-                TP += 1
-            elif delta == 1:
-                FN += 1
-            else:
-                FP += 1
-        F1 = 2*TP/(2*TP+FN+FP)
-        f1_train.append(F1)
-        #print("Number of errors is " + str(FN+FP) + " out of " + str(len(X_train)) + "images")
-        #print("Training F1 is " + str(F1))
+TP = 0
+FN = 0
+FP = 0
+for i in range(len(y_val_pred)):
+    if y_val[i] == 1:
+        if y_val_pred[i] == 1:
+            TP += 1
+        else:
+            FN += 1
+    else:
+        if y_val_pred[i] == 1:
+            FP += 1
+        
+F1 = 2*TP/(2*TP+FN+FP)
+print("Number of errors is " + str(FN+FP) +" (" +str(float(100*(FN+FP)/len(X_val))) + "%)")
+print("F1 on validation is " + str(F1))
 
-        y_pred = clf.predict(X_val)
-        TP = 0
-        FN = 0
-        FP = 0
-        for i in range(len(y_pred)):
-            i = int(i)
-            delta = y_val[i] - y_pred[i]
-            if delta == 0:
-                TP += 1
-            elif delta == 1:
-                FN += 1
-            else:
-                FP += 1
-        F1 = 2*TP/(2*TP+FN+FP)
-        f1_val.append(F1)
-        #print("Number of errors is " + str(FN+FP) + " out of " + str(len(X_val)) + "images")
-        #print("Validation F1 is " + str(F1))
+#plt.show()
 
-plt.plot(f1_train,color = "blue",label = "Train")
-plt.plot(f1_val  ,color = "red" ,label = "Validation")
-plt.legend()
-plt.xlabel("Number of random states")
-plt.ylabel("F1")
-plt.title("F1 metric versus number of random states(Random Decision Tree)")
-plt.show()
 
